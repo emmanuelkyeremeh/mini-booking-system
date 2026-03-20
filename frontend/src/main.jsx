2c030c3e65c19ee7842e18861e05e075b2e9fcb1
Emmanuel Kyeremeh <josephkyeremeh53@gmail.com>
2026-03-20 14:43:07 +0000

    Fix case-sensitive css import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
-import "./app.css";
+import "./App.css";
 import App from './App.jsx'
 import { AuthProvider } from "./auth/AuthProvider.jsx";
 import "react-datepicker/dist/react-datepicker.css";
